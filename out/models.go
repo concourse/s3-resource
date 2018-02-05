ced1eb23f44a2afa36576a1dedc8f85072aee175
cappyzawa <shu.920921@gmail.com>
2018-02-05 22:09:49 +0900

    remove package name from func name's prefix around out

--- out/models.go
+++ out/models.go
@@ -4,7 +4,7 @@ import (
 	"github.com/concourse/s3-resource"
 )
 
-type OutRequest struct {
+type Request struct {
 	Source s3resource.Source `json:"source"`
 	Params Params            `json:"params"`
 }
@@ -17,7 +17,7 @@ type Params struct {
 	ContentType string `json:"content_type"`
 }
 
-type OutResponse struct {
+type Response struct {
 	Version  s3resource.Version        `json:"version"`
 	Metadata []s3resource.MetadataPair `json:"metadata"`
 }
