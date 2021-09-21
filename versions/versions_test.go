aa4fc634ea05a80fb504b9af11b6af6dde36586a
Olivier Dormond <olivier.dormond@pix4d.com>
2021-09-21 16:40:47 +0200

    Add test of new check implementation
    Signed-off-by: Olivier Dormond <olivier.dormond@pix4d.com>

--- versions/versions_test.go
+++ versions/versions_test.go
@@ -1,6 +1,10 @@
 package versions_test
 
 import (
+	"errors"
+
+	s3resource "github.com/concourse/s3-resource"
+	"github.com/concourse/s3-resource/fakes"
 	"github.com/concourse/s3-resource/versions"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -134,6 +138,171 @@ var _ = Describe("Extract", func() {
 			Ω(result.Path).Should(Equal("abc-1.0.5-def-2.3.4.tgz"))
 			Ω(result.Version.String()).Should(Equal("2.3.4"))
 			Ω(result.VersionNumber).Should(Equal("2.3.4"))
+		})
+	})
+})
+
+var _ = Describe("GetMatchingPathsFromBucket", func() {
+	var s3client *fakes.FakeS3Client
+
+	BeforeEach(func() {
+		s3client = &fakes.FakeS3Client{}
+	})
+
+	Context("When the regexp has no '/'", func() {
+		Context("when the regexp has no special char", func() {
+			It("uses only the empty string as prefix", func() {
+				versions.GetMatchingPathsFromBucket(s3client, "bucket", "regexp")
+				Ω(s3client.ChunkedBucketListCallCount()).Should(Equal(1))
+				_, prefix, _ := s3client.ChunkedBucketListArgsForCall(0)
+				Ω(prefix).Should(Equal(""))
+			})
+		})
+		Context("when the regexp has a special char", func() {
+			It("uses only the empty string as prefix", func() {
+				versions.GetMatchingPathsFromBucket(s3client, "bucket", "reg.xp")
+				Ω(s3client.ChunkedBucketListCallCount()).Should(Equal(1))
+				_, prefix, _ := s3client.ChunkedBucketListArgsForCall(0)
+				Ω(prefix).Should(Equal(""))
+			})
+		})
+	})
+
+	Context("When regexp special char appears close to the leaves", func() {
+		It("starts directly with the longest prefix", func() {
+			versions.GetMatchingPathsFromBucket(
+				s3client, "bucket", "regexp/will/appear/only/close/tw?o+/leaves",
+			)
+			Ω(s3client.ChunkedBucketListCallCount()).Should(Equal(1))
+			_, prefix, _ := s3client.ChunkedBucketListArgsForCall(0)
+			Ω(prefix).Should(Equal("regexp/will/appear/only/close/"))
+		})
+
+		It("follows only the matching prefixes", func() {
+			s3client.ChunkedBucketListReturnsOnCall(0, s3resource.BucketListChunk{
+				CommonPrefixes: []string{
+					"regexp/will/appear/only/close/from/",
+					"regexp/will/appear/only/close/to/",
+					"regexp/will/appear/only/close/too/",
+					"regexp/will/appear/only/close/two/",
+				},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(1, s3resource.BucketListChunk{
+				Paths: []string{
+					"regexp/will/appear/only/close/to/the-end",
+					"regexp/will/appear/only/close/to/leaves",
+				},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(2, s3resource.BucketListChunk{
+				CommonPrefixes: []string{"regexp/will/appear/only/close/too/late/"},
+				Paths:          []string{"regexp/will/appear/only/close/too/soon"},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(3, s3resource.BucketListChunk{
+				Paths: []string{
+					"regexp/will/appear/only/close/two/three",
+				},
+			}, nil)
+
+			matchingPaths, err := versions.GetMatchingPathsFromBucket(
+				s3client, "bucket", "regexp/will/appear/only/close/tw?o+/leaves",
+			)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(s3client.ChunkedBucketListCallCount()).Should(Equal(4))
+			for idx, expectedPrefix := range []string{
+				"regexp/will/appear/only/close/",
+				"regexp/will/appear/only/close/to/",
+				"regexp/will/appear/only/close/too/",
+				"regexp/will/appear/only/close/two/",
+			} {
+				_, prefix, _ := s3client.ChunkedBucketListArgsForCall(idx)
+				Ω(prefix).Should(Equal(expectedPrefix))
+			}
+			Ω(matchingPaths).Should(ConsistOf("regexp/will/appear/only/close/to/leaves"))
+		})
+	})
+
+	Context("When there are too many leaves for a single request", func() {
+		It("continues requesting more", func() {
+			s3client.ChunkedBucketListReturnsOnCall(0, s3resource.BucketListChunk{
+				Truncated: true,
+				Paths: []string{
+					"prefix/leaf-0",
+					"prefix/leaf-1",
+				},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(1, s3resource.BucketListChunk{
+				Truncated: false,
+				Paths: []string{
+					"prefix/leaf-2",
+					"prefix/leaf-3",
+				},
+			}, nil)
+
+			matchingPaths, err := versions.GetMatchingPathsFromBucket(
+				s3client, "bucket", "prefix/leaf-(.*)",
+			)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(matchingPaths).Should(ConsistOf(
+				"prefix/leaf-0",
+				"prefix/leaf-1",
+				"prefix/leaf-2",
+				"prefix/leaf-3",
+			))
+		})
+	})
+
+	Context("When there are too many prefixes for a single request", func() {
+		It("continues requesting more", func() {
+			s3client.ChunkedBucketListReturnsOnCall(0, s3resource.BucketListChunk{
+				Truncated: true,
+				CommonPrefixes: []string{
+					"prefix-0/",
+					"prefix-1/",
+				},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(1, s3resource.BucketListChunk{
+				Truncated: false,
+				CommonPrefixes: []string{
+					"prefix-2/",
+					"prefix-3/",
+				},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(2, s3resource.BucketListChunk{
+				Paths: []string{"prefix-0/leaf-0"},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(3, s3resource.BucketListChunk{
+				Paths: []string{"prefix-1/leaf-1"},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(4, s3resource.BucketListChunk{
+				Paths: []string{"prefix-2/leaf-2"},
+			}, nil)
+			s3client.ChunkedBucketListReturnsOnCall(5, s3resource.BucketListChunk{
+				Paths: []string{"prefix-3/leaf-3"},
+			}, nil)
+
+			matchingPaths, err := versions.GetMatchingPathsFromBucket(
+				s3client, "bucket", "prefix-\\d+/leaf-(.*)",
+			)
+			Ω(err).ShouldNot(HaveOccurred())
+			Ω(matchingPaths).Should(ConsistOf(
+				"prefix-0/leaf-0",
+				"prefix-1/leaf-1",
+				"prefix-2/leaf-2",
+				"prefix-3/leaf-3",
+			))
+		})
+	})
+
+	Context("When S3 returns an error", func() {
+		BeforeEach(func() {
+			s3client.ChunkedBucketListReturns(
+				s3resource.BucketListChunk{},
+				errors.New("S3 failure"),
+			)
+		})
+		It("fails", func() {
+			_, err := versions.GetMatchingPathsFromBucket(s3client, "bucket", "dummy")
+			Ω(err).Should(HaveOccurred())
 		})
 	})
 })
